57a1a1137b2f970a2afb3adedeb8b6684374b285
Jacob Janak <jakeathotmail@gmail.com>
2018-06-06 17:13:26 -0700

    Made it better. Removed all my tests that didn't even work.

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-let number = 6; // customize this!
-let numberSquared = number ** 2; // square of user number
+// Global Variables
+const number = process.argv[2] || 6; // customize this!
+const numberSquared = number ** 2; // square of user number
 
-//console.log(clockwiseSpiral(number))
+// Script
+const spiral = clockwiseSpiral(number);
+printSpiral(spiral, numberSquared)
 
 function clockwiseSpiral(number) {
   let i, j; // looping
   let count = 1; // current number
   let currentSideCount = number; // number of additions added per side (decrements)
 
-  let sides = ['t', 'r', 'b', 'l']; // list of sides on a square
+  const sides = ['t', 'r', 'b', 'l']; // list of sides on a square
+  const spiral = createBlankArray(); // holds the grid
   let currentSideIndex = 0; // tells us which side we're currently on
-  let spiral = createBlankArray(); // holds the grid
-  let spiralLayer = -1; // current layer of depth
+  let spiralLayer = -1; // current layer of depth (actually starts at 0)
 
   // this object will add numbers to the grid depending on which side we want
-  let addNumbers = {
+  const addNumbers = {
     toTop: () => {
       spiralLayer++;
       for (i = spiralLayer; i < spiralLayer + currentSideCount; i++) {
@@ -63,9 +66,9 @@ function clockwiseSpiral(number) {
 
   // create an n by n array grid of zero's
   function createBlankArray() {
-    let iArray = [];
+    const iArray = [];
     for (i = 0; i < number; i++) {
-      let jArray = [];
+      const jArray = [];
       for (j = 0; j < number; j++) {
         jArray.push(0)
       }
@@ -73,8 +76,26 @@ function clockwiseSpiral(number) {
     }
     return iArray;
   }
-
   return spiral;
 }
 
-module.exports = clockwiseSpiral;
+function printSpiral(spiral) {
+  // use the number of digits to space nums evenly
+  const numOfDigits = (num) => String(num).length;
+  const maxDigits = numOfDigits(numberSquared);
+
+  let str = '';
+  spiral.forEach((row) => {
+    row.forEach((num) => {
+      const digits = numOfDigits(num);
+      for (i = 0; i < maxDigits - digits; i++) {
+        str += ' ';
+      }
+      // adding at least 1 space to all nums
+      str += ' ' + num
+    })
+    // adding a new line character
+    str += '\n'
+  })
+  console.log(str)
+}
